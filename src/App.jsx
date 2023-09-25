e0a06f45c85b3e6681b7777da389eb0d5b099084
Guilherme <guisilvabarr@gmail.com>
2023-09-25 16:05:38 -0300

    inicio de la utilizacion de API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Window from './components/window/Window';
 import Modal from './components/modal/Modal';
 import Search from './components/search/Search';
 import './App.css';
 
 function App() {
+
+  // mostrar o ocultar Search/Modal
   const [modalVisible, setModalVisible] = useState(false);
   const [searchVisible, setSearchVisible] = useState(true);
 
@@ -17,7 +20,33 @@ function App() {
     setModalVisible(false);
     setSearchVisible(true);
   };
- 
+
+  //Ubicación del usuario
+  const [location, setLocation] = useState(false);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      getWeather(position.coords.latitude, position.coords.longitude);
+      setLocation(true)
+    })
+  }, []);
+
+  //guardar los datos de la API
+  const [weather, setWeather] = useState(false);
+
+  let getWeather = async (lat, long) => {
+    let res = await axios.get("https://api.openweathermap.org/data/2.5/weather", {
+      params: {
+        lat: lat,
+        lon: long,
+        appid: process.env.REACT_APP_OPEN_WEATHER_KEY,
+        lang: 'en',
+        units: 'metric'
+      }
+    });
+    setWeather(res.data);
+  }
+
   return (
     <div className='general'>
       {searchVisible && <Search onClick={handleSearchClick} />}
